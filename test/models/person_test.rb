046cd57c794cc53ad736d476698907296bb009c5
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-30 18:24:20 +0900

    add validation test.

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "if first_name is blank, validation is false" do
+    person = Person.new(first_name: '', last_name: 'last_name')
+    assert_not(person.valid?)
+  end
+
+  test "if last_name is blank, validation is false" do
+    person = Person.new(first_name: 'first', last_name: '')
+    assert_not(person.valid?)
+  end
 end
