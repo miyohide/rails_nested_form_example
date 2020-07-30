4b8a00c7aab4c8d7342fcb08aa535669e60b71ec
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-30 18:09:54 +0900

    add association and validation.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ApplicationRecord
+  has_many :addresses
+  accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: :all_blank
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
