426bd0edc7bf85dd1b94bb69c1a4f62f973b9157
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-30 18:00:39 +0900

    add null option

--- db/migrate/20200730085334_create_addresses.rb
+++ db/migrate/20200730085334_create_addresses.rb
@@ -1,8 +1,8 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.string :kind
-      t.string :street
+      t.string :kind, null: false
+      t.string :street, null: false
       t.references :person, null: false, foreign_key: true
 
       t.timestamps
