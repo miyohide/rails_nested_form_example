b8b1de066666da30d7e9fb5e0ab07c290be06b09
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-30 18:34:06 +0900

    add validation error test for address

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "if kind is blank, validation is false" do
+    address = Address.new(kind: '', street: 'street')
+    assert_not(address.valid?)
+  end
+
+  test "if street is blank, validation is false" do
+    address = Address.new(kind: 'kind', street: '')
+    assert_not(address.valid?)
+  end
 end
