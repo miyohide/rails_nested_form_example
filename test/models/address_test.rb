a0fe258af66c61bb79fc67ad58191712e8756a57
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-30 18:43:45 +0900

    add person attributes

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -2,12 +2,25 @@ require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
   test "if kind is blank, validation is false" do
-    address = Address.new(kind: '', street: 'street')
+    person = Person.new(first_name: 'first', last_name: 'last')
+    address = Address.new(kind: '', street: 'street', person: person)
     assert_not(address.valid?)
   end
 
   test "if street is blank, validation is false" do
-    address = Address.new(kind: 'kind', street: '')
+    person = Person.new(first_name: 'first', last_name: 'last')
+    address = Address.new(kind: 'kind', street: '', person: person)
     assert_not(address.valid?)
+  end
+
+  test "if person is nil, validation is false" do
+    address = Address.new(kind: 'kind', street: '', person: nil)
+    assert_not(address.valid?)
+  end
+
+  test "if all attributes are not blank or nil, validation is true" do
+    person = Person.new(first_name: 'first', last_name: 'last')
+    address = Address.new(kind: 'kind', street: 'street', person: person)
+    assert(address.valid?)
   end
 end
