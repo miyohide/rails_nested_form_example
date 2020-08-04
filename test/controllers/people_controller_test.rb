53e77b4cf33fa3e8bd89f05307fdf2bcdf305683
Hidenori Miyoshi <miyohide@gmail.com>
2020-08-04 18:26:31 +0900

    add some assertion to GET /people controller test.

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
   test "GET /people" do
+    p = FactoryBot.create(:person)
     get "/people"
     assert_response(:success)
+    assert_equal("/people", path)
+    assert_match(p.first_name, @response.body)
+    assert_match(p.last_name, @response.body)
   end
 end
