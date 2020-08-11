41a6b7b37f1a2d7f311535dbe1a3c84a8a3591cd
Hidenori Miyoshi <miyohide@gmail.com>
2020-08-11 21:29:39 +0900

    create ability_hidden_tag method.

--- app/helpers/person_helper.rb
+++ app/helpers/person_helper.rb
@@ -2,4 +2,8 @@ module PersonHelper
   def ability_name_prefix(index)
     "person[abilities_attributes][#{index}]"
   end
+
+  def ability_hidden_tag(index, ability_selection)
+    tag.hidden name: ability_name_prefix(index), value: ability_selection.try(:id)
+  end
 end
--- test/helpers/person_helper_test.rb
+++ test/helpers/person_helper_test.rb
@@ -4,4 +4,14 @@ class PersonHelperTest < ActionView::TestCase
   test "when call ability_name_prefix, return person + abilities_attributes string" do
     assert_equal("person[abilities_attributes][1]", ability_name_prefix(1))
   end
+
+  test "return hidden tag" do
+    assert_equal('<hidden name="person[abilities_attributes][1]"></hidden>', ability_hidden_tag(1, nil))
+  end
+
+  test "return hidden tag with value" do
+    mock = MiniTest::Mock.new.expect(:try, 1, [:id])
+    actual = ability_hidden_tag(1, mock)
+    assert_equal('<hidden name="person[abilities_attributes][1]" value="1"></hidden>', actual)
+  end
 end
