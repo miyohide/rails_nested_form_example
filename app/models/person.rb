543266236cf5fdfeae1de34772ce06f15199c9f1
Hidenori Miyoshi <miyohide@gmail.com>
2020-08-19 22:06:40 +0900

    add comment

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,14 +7,23 @@ class Person < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  # personが持っている各Abilityのインスタンスに対して、以下のキーと値から成る
+  # Hashをかえす
+  #   キー : ability_nameをシンボル化したもの
+  #   値 : Abilityのインスタンス
+  # @return [Hash] キーはability_nameのシンボル、値はAbilityのインスタンス
   def ability_selections
     abilities.map do |record|
       [record.ability_name.to_sym, record]
     end.to_h
   end
 
-  def checked?(ability)
-    ability_selection = ability_selections[ability.to_sym]
+  # データが存在しており、かつ削除対象となっていない（marked_for_destruction?がfalse）
+  # である場合はtrueを、それ以外はfalseを返す。
+  # @param ability_name [String] 対象のability名
+  # @return [Boolean] データが存在し、削除対象となっていない場合はtrue、それ以外はfalse
+  def checked?(ability_name)
+    ability_selection = ability_selections[ability_name.to_sym]
     ability_selection.present? && !ability_selection.marked_for_destruction?
   end
 end
