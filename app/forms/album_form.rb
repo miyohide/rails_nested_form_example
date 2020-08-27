2d5c0d4c0f8f8aa21c642f996f7cba65194c3505
Hidenori Miyoshi <miyohide@gmail.com>
2020-08-27 16:41:38 +0900

    add validation.

--- app/forms/album_form.rb
+++ app/forms/album_form.rb
@@ -1,8 +1,10 @@
 class AlbumForm < Reform::Form
   property :album_title
+  validates :album_title, presence: true
 
   property :artist do
     property :full_name
+    validates :full_name, presence: true
   end
 
   # collection :songs, populate_if_empty: Song do
